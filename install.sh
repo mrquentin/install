443c59a06c2996fc1cb3858d054e13eb496db33c
mrquentin <32539717+mrquentin@users.noreply.github.com>
2022-12-01 21:20:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,11 +29,13 @@ echo "Installing dotfiles ..."
 git clone --bare git@github.com:mrquentin/dotfiles.git $HOME/.dotfiles-cfg
 mkdir -p .config-backup
 dot checkout
+
 if [ $? = 0 ]; then
   echo "Checked out config"
 else
   echo "Backing up pre-existing dot files ..."
   dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/
-fi
+fi;
+
 dot checkout
 dot config status.showUntrackedFiles no
