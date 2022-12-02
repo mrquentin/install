a093328948c426ec875e13663fe3435f4db76383
mrquentin <32539717+mrquentin@users.noreply.github.com>
2022-12-01 21:23:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,11 +31,11 @@ mkdir -p .config-backup
 dot checkout
 
 if [ $? = 0 ]; then
-  echo "Checked out config";
-else
-  echo "Backing up pre-existing dot files ...";
-  dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/
-fi;
+  echo "Checked out config"
+  else
+    echo "Backing up pre-existing dot files ..."
+    dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/
+fi
 
 dot checkout
 dot config status.showUntrackedFiles no
