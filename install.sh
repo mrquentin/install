cdcf193fd55c235cbcbb636aa6c3b8b387f3b4e0
Quentin Levasseur <32539717+mrquentin@users.noreply.github.com>
2022-12-02 00:57:15 +0000

    fix

--- install.sh
+++ install.sh
@@ -29,6 +29,8 @@ git clone --bare https://github.com/mrquentin/dotfiles.git $HOME/.dotfiles-cfg
 alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles-cfg --work-tree=$HOME'
 # Backup conflicting files
 mkdir -p .config-backup && dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+# Add alias to manipulate project
+alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles-cfg --work-tree=$HOME'
 # Checkout dotfiles
 dot checkout
 # Configure project to not show untracked files
